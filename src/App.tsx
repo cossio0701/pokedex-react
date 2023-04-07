dced092bccd86a78d995f6ef628a3197f108faa4
CompanyCossio <90640996+CompanyCossio@users.noreply.github.com>
2023-04-06 20:48:30 -0500

    connection to the pokeapi and send a request to get the first ten pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { pokeApiClient } from "./api/pokeApiClient";
+
 function App() {
+  const { data, isLoading } = useQuery(["pokemon-list"], async () => {
+    const response = await pokeApiClient.get("/pokemon?limit=10&offset=0");
+
+    return response.data;
+  });
+
+  console.log(data, isLoading);
+
   return (
     <div className="App">
       <h1 className="text-2xl">Emma es gay</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
